7e40732a36bb1d0d5265d5f9889db2d55843a1d9
alizeekastler <alizeekastler@hotmail.com>
2024-02-19 11:10:40 +0000

    organised behaviour into folders

---
+++ Behaviour/Noxious/1.Tracking_SP.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+"""
+Fish tracking from a video and after defining ROIs in Bonsai
+"""                        
+# Set Library Path 
+lib_path = r'C:\Repos\Social_Pain\libs'
+import sys
+sys.path.append(lib_path)
+
+# Set Base Path
+#base_path = r'S:\WIBR_Dreosti_Lab\Tom\Videos_Alizee'
+base_path =  r'S:\WIBR_Dreosti_Lab\Alizee\Behaviour_Heat_Gradient/NewChamber'
+
+# Import useful libraries
+import os
+import glob
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import scipy.signal as signal
+import scipy.misc as misc
+from scipy import stats
+
+# Import local modules
+import SP_video_TRARK as SPV
+import BONSAI_ARK
+import SP_utilities as SPU
+
+# Read Folder List
+FolderlistFile = base_path + '/Gradient_NewChamber38/Experiments/2023_12_06/Folderlist.txt'
+groups, ages, folderNames, fishStatus = SPU.read_folder_list(FolderlistFile)
+
+# Divisor and closing kernel parameters (thresholding for mask and background)
+divisor=7
+kernelWidth=3
+
+# Bulk analysis of all folders
+for idx,folder in enumerate(folderNames):
+    
+    # Get Folder Names
+    NS_folder, S_folder, Analysis = SPU.get_folder_names(folder)
+
+    # ---------------------
+    # Process Video (Non_Social)
+    bonsaiFiles = NS_folder + r'\Bonsai_ROI_Analysis.bonsai'
+    ROIs = BONSAI_ARK.read_bonsai_crop_rois(bonsaiFiles)
+    # Run tracking in SP_video 
+    fxS, fyS, bxS, byS, exS, eyS, areaS, ortS, motS = SPV.fish_tracking(NS_folder, S_folder, ROIs, divisor=divisor, kSize=kernelWidth)
+
+    # Save Tracking (Non_Social)
+    for i in range(0,6):
+        filename = NS_folder + r'\tracking'+ str(i+1) + '.npz'
+        fish = np.vstack((fxS[:,i], fyS[:,i], bxS[:,i], byS[:,i], exS[:,i], eyS[:,i], areaS[:,i], ortS[:,i], motS[:,i]))
+        np.savez(filename, tracking=fish.T)
+    
+    #---------------------
+    # Process Video (Social)
+    bonsaiFiles = S_folder + r'/Bonsai_ROI_Analysis.bonsai'
+    ROIs = BONSAI_ARK.read_bonsai_crop_rois(bonsaiFiles)
+    #Run tracking in SP_video
+    fxS, fyS, bxS, byS, exS, eyS, areaS, ortS, motS = SPV.fish_tracking(S_folder, S_folder, ROIs, divisor=divisor, kSize=kernelWidth)
+
+    # Save Tracking (Social)
+    for i in range(0,6):
+        filename = S_folder + r'/tracking'+ str(i+1) + '.npz'
+        fish = np.vstack((fxS[:,i], fyS[:,i], bxS[:,i], byS[:,i], exS[:,i], eyS[:,i], areaS[:,i], ortS[:,i], motS[:,i]))
+        np.savez(filename, tracking=fish.T)
+         
+    
+    #Close Plots
+    plt.close('all')
+    
+    
+# FIN
+    
--- README.md
+++ README.md
@@ -1,6 +1,17 @@
 # Social_Pain
 Lonely and hurting (fish)
 
-## Python code
+## Behaviour 
 
-Here.
+Gradient : Analysis of 2 different movies of the same fish comparing conditions to a baseline 
+
+0. Pre-process the videos to have a first idea of average position and make sure tracking can be done correctly 
+1. Track 6 fish at a time in 6 individual wells and extract, body, eye and orientation parameters for each frame
+2. Extract information from the tracking data and store into summary npz files for each fish 
+3. Analyse and plot the summary data and save the plots in a figure folder 
+4. Compare different conditions directly 
+5. Use kmeans clustering to define clusters of fish based on their average position in the chamber. Generate an excel file with the saved localisation of each fish and their group belonging. 
+6. Analyse and plot parameters for different groups
+7. Create a summary fingerprint comparing the change (incerease or decrease) in analysed parameters
+
+Noxious: Analysis of 1 long movie 
